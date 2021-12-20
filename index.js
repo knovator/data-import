4a221218a91f7cb12911ccc8325c052c941debb6
VimLeSai <desai.vimal250@gmail.com>
2021-12-20 18:09:06 +0530

    build(Docker):  removing mongo dependancies

--- index.js
+++ index.js
@@ -16,21 +16,3 @@ app.listen(port, () => logger.info(`server started on port ${port}`));
  * @public
  */
 module.exports = app;
-
-be2db351ac9b
-dc176bb2a72b
-1a7d4b86b46b
-90cb47edc11e
-77ff01842285
-adf76923a247
-d1d1b035a00e
-9e5f556851cd
-fcfa7883dccb
-705c7ea31541
-03713a72537e
-0da57582c2d1
-b23dcdaee3e3
-b12c716fdae0
-5ada93c1d31c
-9b2be4277af6
-332c685c1f14
